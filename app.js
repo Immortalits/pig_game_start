20f7cb303283f853912554b19d9dc4ca73a13bf4
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-05-06 17:35:04 +0200

    homework3 done

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ function init() {
 
   // a játék kezdetekor a kockát eltüntetjük:
   // inline style-t adunk hozzá az img-hez...
-  document.querySelector('.dice').style.display = 'none';
+  // document.querySelector('.dice').style.display = 'none';
+  document.querySelector('#dice-1').style.display = 'none';
+  document.querySelector('#dice-2').style.display = 'none';
   // a gombokat megjelenítjük
   document.querySelector('.btn-roll').style.display = 'block';
   document.querySelector('.btn-hold').style.display = 'block';
@@ -46,16 +48,25 @@ document.querySelector('.btn-new').addEventListener('click', init);
 // ha a roll dice gombra kattint a user
 document.querySelector('.btn-roll').addEventListener('click', function () {
   // generálunk egy random számot 1 és 6 között
-  let dice = Math.floor(Math.random() * 6) + 1;
+  // let dice = Math.floor(Math.random() * 6) + 1;
+  let dice1 = Math.floor(Math.random() * 6) + 1;
+  let dice2 = Math.floor(Math.random() * 6) + 1;
   // az eredményt megjelenítjük a UI-on:
-  let diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
+  // let diceDOM = document.querySelector('.dice');
+  // diceDOM.style.display = 'block';
+  let dice1DOM = document.querySelector('#dice-1');
+  dice1DOM.style.display = 'block';
+  let dice2DOM = document.querySelector('#dice-2');
+  dice2DOM.style.display = 'block';
   //string concatenation, szöveg összefűzés (a két + közötti rész beillesztődik a szövegbe)
-  diceDOM.setAttribute('src', 'dice-' + dice + '.png');
+  // diceDOM.setAttribute('src', 'dice-' + dice + '.png');
+  dice1DOM.setAttribute('src', 'dice-' + dice1 + '.png');
+  dice2DOM.setAttribute('src', 'dice-' + dice2 + '.png');
+
   // ha a játékos 1-est dob, a roundScore értékét elveszti és a következő játékos jön
-  if (dice !== 1) {
+  if (dice1 !== 1 && dice2 !== 1) {
     // a dobott értéket kiszámoljuk majd megjelenítjük a piros dobozban...
-    roundScore = roundScore + dice;
+    roundScore = roundScore + dice1 + dice2;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     nextPlayer();
@@ -85,13 +96,15 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
   scores[activePlayer] = scores[activePlayer] + roundScore;
   // update the UI
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-  if (scores[activePlayer] >= 20) {
+  if (scores[activePlayer] >= 100) {
     // játék vége
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
+    // document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
     document.querySelector('.btn-roll').style.display = 'none';
     document.querySelector('.btn-hold').style.display = 'none';
 
